6e29c83969517495c30f1313b9e14e702e7341b7
xielantu <669616212@qq.com>
2023-07-28 21:23:15 +0800

    添加项目文件。

---
+++ 100_8.3小应用.cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct Student
+{
+	int num;
+	char name[32];
+	char sex;
+	int age;
+	double score;
+	char addr[32];
+};
+
+
+
+int main()
+{
+	int a;
+	struct Student stu1;
+	struct Student stu2 = {2,"张三",'g',17,99.5,"北京"};
+
+	a = 10;
+	stu1.num = 1;//点运算符来访问结构体中的成员变量（域）
+	stu1.age = 10;
+	stu1.score = 98.5;
+	strcpy_s(stu1.name, "chenlichen");
+	strcpy_s(stu1.addr, "湖南");
+
+	if (stu1.score > stu2.score)
+	{
+		printf("学习较好的是：\n");
+		printf("学号：%d,年龄：%d,分数：%lf,名字：%s,地址:%s\n",
+			stu1.num, stu1.age, stu1.score, stu1.name, stu1.addr);
+	}
+	else
+	{
+		printf("学习较好的是：\n");
+		printf("学号：%d,年龄：%d,分数：%lf,名字：%s,地址:%s\n",
+			stu2.num, stu2.age, stu2.score, stu2.name, stu2.addr);
+	}
+
+
+
+
+	
+
+
+}
